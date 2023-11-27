e5b0ac0fe063d6c68892ac5a5548b0667b610d6a
anar <anar.sultanov@outlook.com>
2023-11-27 11:54:44 +0400

    nevermind

--- Python 3.0/Task_7.py
+++ Python 3.0/Task_7.py
@@ -1,6 +1,6 @@
 number = int(input("Введите число: "))
 thousands = number // 1000
-hundreds = (number // 100) % 10
+hundreds = (number // 100) %10
 tens = (number // 10) % 10
 units = number % 10
 print (thousands, hundreds, tens, units )
