5851c13bbe50122598760601e71d776213b556d5
S0ul <anar.sultanov@outlook.com>
2023-12-04 01:40:40 +0400

    added new chapter

---
+++ Python 4.0/Testing.py
@@ -0,0 +1,9 @@
+apple = int(input("Доходы от яблок:" ))
+orange = int(input("Доходы от апельсинов:" ))
+income = apple + orange
+if income > 100:
+    print("Достаточный доход.")
+else: 
+    print("Не достаточный доход.")
+
+
