3452f0c8004aa7c4225f409f964c8ea4a0433602
Soul <anar.sultanov@outlook.com>
2024-01-07 11:22:53 +0400

    deleted testing file

--- Python 4.0/Testing.py
+++
@@ -1,9 +0,0 @@
-apple = int(input("Доходы от яблок:" ))
-orange = int(input("Доходы от апельсинов:" ))
-income = apple + orange
-if income > 100:
-    print("Достаточный доход.")
-else: 
-    print("Не достаточный доход.")
-
-
