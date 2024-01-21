af0f5b1022206a131837524fb124ca80f7fcbb09
S0ulek <anar.sultanov@outlook.com>
2024-01-22 01:11:52 +0400

    22.01

--- Python L/sk.py
+++ Python L/sk.py
@@ -140,7 +140,7 @@
 
 #print(user_list)    
 
-word = "itproger, hool, pepka"
+#word = "itproger, hool, pepka"
 #print (word.count(""))
 
 
@@ -151,7 +151,44 @@ word = "itproger, hool, pepka"
 #print(word.isupper())
 
 #print(word.capitalize())
+#hobby = word.split(", ")
+
+#for i in range(len(hobby)):
+ #   hobby[i] = hobby[i].capitalize()
+
+#result = ", ".join(hobby)    
+
+#print(result)    
 
 
-hobby = word.split(", ")
-print(hobby[])
+
+
+
+#country = {"code": "RU", "name": "Russian","population": 144}
+
+#country = dict(code="RU", name="russian")
+
+#for key, value in country.items():
+   # print(key, "-" , value)
+
+#print(country.get("code"))
+
+#country.popitem()
+
+#country["code"] = "none"
+#print(country.items())
+
+
+person = {
+    "user_1":{
+       "first_name":"jonh",
+       "last_name": "marley",
+       "age": 45,
+       "address": ["c.moskow","street tot","45"],
+       "grades": {"math": 5, "physics": 3}}}
+
+
+
+
+
+print(person["user_1"]["address"][1])
