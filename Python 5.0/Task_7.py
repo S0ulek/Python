60e69c5aedaaf687815d539f041206477bab4a54
S0ulek <anar.sultanov@outlook.com>
2024-01-26 02:25:10 +0400

    last task left

--- Python 5.0/Task_7.py
+++ Python 5.0/Task_7.py
@@ -1,8 +1,8 @@
 time = int(input("Введите время:"))
-lunch = (14<= time <=15)
+lunch = (14< time <=15)
 packages = (10<= time <= 12) and (18<= time <= 20)
 relax_time = (22<= time <8)
-if time == (lunch or packages or relax_time):
+if time == lunch or packages or relax_time:
     print("Закрыто")
 else: 
     print("Открыто")
