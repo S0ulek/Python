62998c028d74d9d01ba594f98f2861601ed42e06
S0ulek <anar.sultanov@outlook.com>
2024-01-26 00:27:33 +0400

    3rd task

--- Python 5.0/5.py
+++
@@ -1,6 +0,0 @@
-year = int(input("Введите год выпуска:"))
-speed_count = int(input("Введите количество скоростей:"))
-if year < 2018 or speed_count < 24:
-    print("Не подходит")
-else:
-    print("Подходит")
