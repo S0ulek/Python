520637c04f499d4989b63364c12dad9494d205bc
S0ulek <anar.sultanov@outlook.com>
2024-01-28 00:41:01 +0400

    second answer for 7 task

--- Python 5.0/Task_7.py
+++ Python 5.0/Task_7.py
@@ -1,5 +1,11 @@
+# time = int(input("Введите время:"))
+# if (14<= time <=15) or (10<= time <= 12) or (18<= time <= 20) or (8>time or time>=22):
+#     print("Посылку получить нельзя")
+# else: 
+#     print("Можно получить посылку")
+
 time = int(input("Введите время:"))
-if (14<= time <=15) or (10<= time <= 12) or (18<= time <= 20) or (8>time or time>=22):
-    print("Закрыто")
-else: 
-    print("Открыто")
+if (8<= time <10) or (12<= time <14) or (15<time<18) or (20<=time<22):
+    print("Можно получить посылку")
+else:
+    print("Посылку получить нельзя")
