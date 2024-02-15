ca35c8284ad2814fb7ef11d39dce8bcac0784f25
S0ulek <anar.sultanov@outlook.com>
2024-02-16 02:23:57 +0400

    task 7

---
+++ Python 6.0/Task_6.py
@@ -0,0 +1,9 @@
+deposit = int(input("Вклад в банк:"))
+percent = int(input("Введите процент:"))
+total = int(input("Нужная сумма:"))
+
+year = 0
+while deposit < total:    
+    deposit += (deposit*(percent*0.01))
+    year += 1       
+print ("Прошло",year,"лет")
--- Python 6.0/While.py
+++
@@ -1,8 +0,0 @@
-x = int(input("Вклад в банк:"))
-p = (x/10)-(x%1)
-y = 0
-year = 0
-while y < 6000:
-    y += p
-    year += 1
-print ("Прошло",year,"лет")
