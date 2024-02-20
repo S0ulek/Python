c1874f5923aeb715d25865dbb6231e9884973747
S0ulek <anar.sultanov@outlook.com>
2024-02-21 02:57:18 +0400

    new chapter

--- Python 7.0/task_2.py
+++ Python 7.0/task_2.py
@@ -1,8 +1,7 @@
 middle_salary = 0
 test = 0
 for months in range (0, 12):
-    test += 1
-    salary = int(input("Введите зарплату за",test, "этот месяц:" ))
+    salary = int(input("Введите зарплату за этот месяц:" ))
     middle_salary += salary
 print ("Средняя зарплата составляет", middle_salary//12)
     
