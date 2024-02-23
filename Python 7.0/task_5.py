f9cc00d4f2cbac9f63cecfc2bee03f2de5a487c4
S0ulek <anar.sultanov@outlook.com>
2024-02-24 01:55:53 +0400

    ch

--- Python 7.0/task_5.py
+++ Python 7.0/task_5.py
@@ -2,7 +2,7 @@ a = int(input("Введите первое число:"))
 b = int(input("Введите второе число:"))
 x = 0
 for res in range(a, b):            
-       a%3 >= 0
-       a += 1   
-       x += 1         
+       while a%3 >= 0:
+         a += 1   
+         x += 1         
 print (x)
