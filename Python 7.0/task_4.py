93fc4e17ca79d026aade116215b0950289ccf496
S0ulek <anar.sultanov@outlook.com>
2024-02-28 02:30:21 +0400

    Task 5

--- Python 7.0/task_4.py
+++ Python 7.0/task_4.py
@@ -1,3 +1,21 @@
-excellent = int(input("Введите число Отличников:"))
-good = int(input("Введите число хорошистов:"))
-normal = int(input("Введите число троечников:"))
+students = int(input("Введите число учеников:"))
+normal = 0
+good = 0
+excellent = 0
+stud_num = 0
+for rating in range(students):
+    stud_num += 1
+    print ("Какую оценку получил ученик номер", stud_num)
+    r = int(input())
+    if r == 3:
+        normal += 1
+    elif r == 4:
+        good += 1
+    elif r == 5:
+        excellent += 1
+if normal < excellent > good:
+    print ("Сегодня больше отличников")
+elif excellent < good > normal:
+    print ("Сегодня больше хорошистов")
+elif excellent < normal > good:
+    print("Сегодня больше норм")
