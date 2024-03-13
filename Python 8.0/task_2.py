d6c11e1e32b66070c54b52a38e67614395e7ec4b
S0ulek <anar.sultanov@outlook.com>
2024-03-13 23:10:19 +0400

    8th finish

--- Python 8.0/task_2.py
+++ Python 8.0/task_2.py
@@ -2,6 +2,6 @@ debtor = int(input("Введите число должников:"))
 debt_sum = 0
 for client in range (0, debtor, 5):
     print("Должник с номером", client)
-    debt = int(input("Ваша сумма долга:"))
+    debt = int(input("Сколько вы должны? "))
     debt_sum += debt
 print("Общая сумма долга:", debt_sum)
--- Python 8.0/task_3.py
+++ Python 8.0/task_3.py
@@ -1,9 +1,12 @@
 reverse_timer = 60
+timer = 0
 for interval in range (reverse_timer,0, -5):
     print("Осталось", interval,"секунд.")
     check = int(input("Вы хотите остановить разогрев? 0/1:"))
     if check:
-        print("Прошло", interval, "секунд","\nВаша еда готова можете забрать.")
+        print("Прошло", reverse_timer, "секунд","\nВаша еда готова, можете забрать.")
         break
+    else: 
+         reverse_timer -= interval
 else:
         print("Ваша еда готова, осторожно горячо.")  
